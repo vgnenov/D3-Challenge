af0464006d9eff33064db9349fb28a9864c5c585
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-15 23:18:26 -0500

    margins

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,3 +1,11 @@
 // create svgHeight and svgWidth variables 
 var svgWidth = 960;
 var svgHeight = 600;
+
+// create the margin variable
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 50
+  };

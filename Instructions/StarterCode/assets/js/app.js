45c48acd88ea6c000bcf997eff84455f0a7c3b65
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-15 23:27:33 -0500

    updated notes in js file

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,3 +1,8 @@
+// Note: You'll need to use python -m http.server to run the visualization. 
+// This will host the page at localhost:8000 in your web browser.
+
+
+
 // create svgHeight and svgWidth variables 
 var svgWidth = 960;
 var svgHeight = 600;

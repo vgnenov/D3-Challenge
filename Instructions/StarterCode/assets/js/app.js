1601b1b53c3c2193402ed26f27a3fb47f582e593
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-15 23:24:24 -0500

    chart attributes part 2

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -80,3 +80,41 @@ d3.csv("data.csv").then(function(healthDataPoints){
         .attr("cy", d=> yScale(d.healthcare))
         .attr("r", 16)
         .attr("fill", "lightblue")
+
+           // add the text to the circles
+    var textSelection = chartGroup.selectAll('.text')
+    console.log(textSelection)
+        textSelection.data(healthDataPoints)
+        .enter()
+        .append("text")
+        .classed('text', true)
+        .attr("x", d => xScale(d.poverty))
+        .attr("y", d => yScale(d.healthcare))
+        .attr("transform", `translate(-10, 6)`)
+        .text(d => {
+            return d.abbr
+        })
+        .style("fill", "white" )
+
+
+    // create a label group for x and y abels    
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20 })`);
+
+    // create x label variable
+    var xLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .text("In Poverty (%) ")
+        .style("font-weight", "bold")
+
+    
+    // create y label variable
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0 - (height/2))
+        .attr("y", 0 - margin.left)
+        .attr("dy", "1em")
+        .text("Locks Healthcare (%)")
+        .style("font-weight", "bold")
+});
